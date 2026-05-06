ce9031527747628c3829a73463cf10ef272a4c4c
MihaPilat <82609077+MihaPilat@users.noreply.github.com>
2026-05-06 13:24:08 +0300

    fix: level up logic and experience doubling bug

--- SurvivalArena2D/Assets/--Project--/Scripts/Experience/ExperienceOrb.cs
+++ SurvivalArena2D/Assets/--Project--/Scripts/Experience/ExperienceOrb.cs
@@ -2,14 +2,18 @@ using UnityEngine;
 
 public class ExperienceOrb : MonoBehaviour
 {
+    private bool _isCollected = false;
     private int _expAmount;
 
     public void Init(int amount) => _expAmount = amount;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
+        if (_isCollected) return;
+
         if (other.TryGetComponent<Character>(out var player))
         {
+            _isCollected = true;
             player.AddExperience(_expAmount);
             Destroy(gameObject);
         }
