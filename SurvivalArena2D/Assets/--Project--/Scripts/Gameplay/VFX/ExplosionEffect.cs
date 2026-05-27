044153c0ba17f6e62107956369ce831d5d186e6c
MihaPilat <82609077+MihaPilat@users.noreply.github.com>
2026-05-27 14:55:04 +0300

    feat(effects): implement dynamic scaling for explosion particles based on radius

--- SurvivalArena2D/Assets/--Project--/Scripts/Gameplay/VFX/ExplosionEffect.cs
+++ SurvivalArena2D/Assets/--Project--/Scripts/Gameplay/VFX/ExplosionEffect.cs
@@ -3,13 +3,15 @@ using UnityEngine;
 [RequireComponent(typeof(ParticleSystem))]
 public class ExplosionEffect : MonoBehaviour
 {
-    private ParticleSystem _particleSystem;
+    private ParticleSystem _mainParticleSystem;
+    private ParticleSystem[] _childParticleSystems;
     private PoolFactory _poolFactory;
     private ExplosionEffect _originPrefab;
 
     private void Awake()
     {
-        _particleSystem = GetComponent<ParticleSystem>();
+        _mainParticleSystem = GetComponent<ParticleSystem>();
+        _childParticleSystems = GetComponentsInChildren<ParticleSystem>();
     }
     public void SetPoolData(ExplosionEffect prefab, PoolFactory factory)
     {
@@ -20,14 +22,48 @@ public class ExplosionEffect : MonoBehaviour
     {
         transform.position = position;
 
-        var shape = _particleSystem.shape;
-        shape.radius = radius;
+        foreach (var ps in _childParticleSystems)
+        {
+            if (ps == null) continue;
 
-        var main = _particleSystem.main;
-        main.startColor = explosionColor;
+            var main = ps.main;
+            main.startColor = explosionColor;
 
-        _particleSystem.Play();
-        Invoke(nameof(ReturnToPool), main.duration + main.startLifetime.constantMax);
+            var shape = ps.shape;
+            if (shape.enabled)
+            {
+                shape.radius = radius;
+            }
+
+            if (ps == _mainParticleSystem || ps.gameObject.name == "Shockwave")
+            {
+                main.startSize = radius * 2f;
+            }
+
+            // Настройка для Искр (Sparks)
+            if (ps.gameObject.name == "Sparks")
+            {
+                main.startSpeed = new ParticleSystem.MinMaxCurve(radius * 3f, radius * 5f);
+
+                int dynamicCount = Mathf.RoundToInt(15 + (radius * 20f));
+
+                var emission = ps.emission;
+                if (emission.burstCount > 0)
+                {
+                    ParticleSystem.Burst[] bursts = new ParticleSystem.Burst[emission.burstCount];
+                    emission.GetBursts(bursts);
+
+                    bursts[0].count = dynamicCount;
+
+                    emission.SetBursts(bursts);
+                }
+            }
+        }
+
+        _mainParticleSystem.Play(withChildren: true);
+
+        CancelInvoke(nameof(ReturnToPool));
+        Invoke(nameof(ReturnToPool), _mainParticleSystem.main.duration + 0.5f);
     }
 
     private void ReturnToPool()
